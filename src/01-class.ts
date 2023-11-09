09bfad779966f6250f0dba366c2e5c72e200e867
afperdomo2 <alvarado0925@gmail.com>
2023-11-09 13:24:44 -0500

    01-class: Implementando propiedades y métodos privados en la clase

--- src/01-class.ts
+++ src/01-class.ts
@@ -1,7 +1,7 @@
 export class MyDate {
-  public year: number;
-  public month: number;
-  public day: number;
+  private year: number;
+  private month: number;
+  private day: number;
 
   constructor(year: number, month: number, day: number) {
     this.year = year;
@@ -9,27 +9,38 @@ export class MyDate {
     this.day = day;
   }
 
-  public printFormat(): string {
-    return `${this.day}/${this.month}/${this.year}`;
+  private addPadding(value: number): string {
+    return value < 10 ? `0${value}` : `${value}`;
   }
 
-  public add(amount: number, type: 'days' | 'months' | 'years') {
+  printFormat(): string {
+    const day = this.addPadding(this.day);
+    const month = this.addPadding(this.month);
+    return `${day}/${month}/${this.year}`;
+  }
+
+  add(amount: number, type: 'days' | 'months' | 'years') {
     if (type === 'days') this.day += amount;
     if (type === 'months') this.month += amount;
     if (type === 'years') this.year += amount;
   }
+
+  getDay() {
+    return this.day;
+  }
+
+  getMonth() {
+    return this.month;
+  }
+
+  getYear() {
+    return this.year;
+  }
 }
 
-const myDate = new MyDate(2023, 11, 9);
+const myDate = new MyDate(2023, 2, 9);
 console.info('printFormat:', myDate.printFormat());
 myDate.add(2, 'days');
 console.info('printFormat:', myDate.printFormat());
 myDate.add(10, 'years');
 console.info('printFormat:', myDate.printFormat());
-
-/**
- * Al ser públicos los las propiedades, se pueden mutar y acceder
- * desde afuera de la clase
- */
-myDate.year = 2999;
-console.info(myDate.year);
