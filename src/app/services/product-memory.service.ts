829b4a82aa122d52b8e453acc6076e74987b4f35
afperdomo2 <alvarado0925@gmail.com>
2023-11-17 17:09:24 -0500

    App: Crea la clase para el servicio de ProductMemoryService

---
+++ src/app/services/product-memory.service.ts
@@ -0,0 +1,43 @@
+import { faker } from '@faker-js/faker';
+import { CreateProductDto, UpdateProductDto } from '../dtos/product.dto';
+import { Product } from '../models/product.model';
+
+export class ProductMemoryService {
+  private products: Product[] = [];
+
+  create(data: CreateProductDto) {
+    const newProduct: Product = {
+      ...data,
+      id: faker.number.int(),
+      creationAt: faker.date.recent(),
+      updatedAt: faker.date.recent(),
+      category: {
+        id: data.categoryId,
+        name: faker.commerce.department(),
+        creationAt: faker.date.recent(),
+        updatedAt: faker.date.recent(),
+        image: faker.image.url(),
+      },
+    };
+    this.add(newProduct);
+  }
+
+  add(product: Product) {
+    this.products.push(product);
+    return product;
+  }
+
+  update(id: Product['id'], changes: UpdateProductDto): Product {
+    const index = this.products.findIndex((item) => item.id === id);
+    const prevData = this.products[index];
+    this.products[index] = {
+      ...prevData,
+      ...changes,
+    };
+    return this.products[index];
+  }
+
+  findOne(id: Product['id']) {
+    return this.products.find((product) => product.id === id);
+  }
+}
